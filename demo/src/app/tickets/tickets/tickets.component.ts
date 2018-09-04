961ec441e8d96232aa4f8ce6091e4524f3296276
TJ VanToll <tj.vantoll@gmail.com>
2018-09-04 15:22:31 -0400

    Initial ticket implementation

--- demo/src/app/tickets/tickets/tickets.component.ts
+++ demo/src/app/tickets/tickets/tickets.component.ts
@@ -5,15 +5,20 @@ import { BackendService, Ticket } from '../../backend.service';
 @Component({
   selector: 'app-tickets',
   templateUrl: './tickets.component.html',
+  styleUrls: ["./tickets.component.css"]
 })
 export class TicketsComponent implements OnInit {
   tickets: Ticket[] = [];
+  listLoaded;
+  isLoading;
 
   constructor(
     private zone: NgZone,
     private backendService: BackendService) { }
 
   ngOnInit() {
+    this.listLoaded = false;
+    this.isLoading = true;
     this.zone.run(
       () => this.loadData()
     );
@@ -21,5 +26,17 @@ export class TicketsComponent implements OnInit {
 
   async loadData() {
     this.tickets = await this.backendService.getTickets();
+    this.isLoading = false;
+    this.listLoaded = true;
+  }
+
+  getStatusColor(status) {
+    if (status == "Completed" || status == "Resolved") {
+      return "#00880A";
+    } else if (status == "Open") {
+      return "#DDAA00";
+    } else {
+      return "#D63100";
+    }
   }
 }
