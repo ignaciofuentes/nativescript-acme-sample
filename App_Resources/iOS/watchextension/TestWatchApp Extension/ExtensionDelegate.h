477c2f820934aadab5997c306bc8adfdd25c8543
TJ VanToll <tj.vantoll@gmail.com>
2019-07-08 16:07:31 -0400

    Initial attempt at adding a watchOS app

---
+++ App_Resources/iOS/watchextension/TestWatchApp Extension/ExtensionDelegate.h
@@ -0,0 +1,13 @@
+//
+//  ExtensionDelegate.h
+//  TestWatchApp Extension
+//
+//  Created by TJ on 7/8/19.
+//  Copyright © 2019 Telerik. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+@interface ExtensionDelegate : NSObject <WKExtensionDelegate>
+
+@end
---
+++ App_Resources/iOS/watchextension/TestWatchApp Extension/NotificationController.h
@@ -0,0 +1,14 @@
+//
+//  NotificationController.h
+//  TestWatchApp Extension
+//
+//  Created by TJ on 7/8/19.
+//  Copyright © 2019 Telerik. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface NotificationController : WKUserNotificationInterfaceController
+
+@end
